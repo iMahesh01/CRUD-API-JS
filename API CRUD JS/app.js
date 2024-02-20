739a34a1afc48da1d9396cd0e171b5e6e649625e
iMahesh01 <96873662+iMahesh01@users.noreply.github.com>
2024-02-20 14:09:14 +0530

    Update app.js

--- API CRUD JS/app.js
+++ API CRUD JS/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-const url = 'mongodb://127.0.0.1:27017/AlienDBex'
+const url = 'mongodb://127.0.0.1:27017/Student'
 
 const app = express()
 
